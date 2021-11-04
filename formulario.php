464a76acc46dab277490ce1ae00a95f232c36d02
elopez2021 <93343076+elopez2021@users.noreply.github.com>
2021-11-04 14:02:33 -0400

    pagina de referencia

---
+++ formulario.php
@@ -0,0 +1,50 @@
+<?php
+include("config.php");
+
+
+     
+ ?>
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head> 
+	<style> 
+
+	</style>
+	
+	
+	<meta charset="UTF-8"> 
+	<title>Tokyo</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200&display=swap" rel="stylesheet">
+	<link rel="Shortcut icon" type="image/x-icon" href="imagenes/asd.ico">
+
+
+</head>
+<body>
+	
+	<header>
+		<div class="logotipo"> 
+			 <h1 class="tilprin">-Tokyo-</h1>
+		</div>
+
+		
+		<nav><a id="Inicio"></a>
+				<ul>
+					<li><a href="#Inicio">Registrate</a> </li>
+					<li><a href="comp.html">Acerca de</a></li>
+					<li><a href="formulario.html">formulario de contacto</a></li>
+					<li><a href="index..html.html">Inicio</a></li>
+				</ul>
+		</nav>
+	</header>
+
+  
+
+</body>
+</html>
+
+
---
+++ tokyo.php
@@ -0,0 +1,199 @@
+﻿
+
+<!DOCTYPE html>
+<html lang="es">
+<head> 
+	<style> 
+	h1 {
+		 color:black;
+		 text-align:center;
+		 font-family: 22px;
+	
+	
+	}
+	h2{color:black;
+		text-align:center;
+		 font-family: 22px;
+		
+		 font-style: italic;	
+		 
+	}
+	
+	  
+	</style>
+	
+	
+	<meta charset="UTF-8"> 
+	<title>Tokyo</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200&display=swap" rel="stylesheet">
+	<link rel="Shortcut icon" type="image/x-icon" href="imagenes/asd.ico">
+
+
+</head>
+<body>
+	
+	<header>
+		<div class="logotipo"> 
+			 <h1 class="tilprin">-Tokyo- pagina wgeb-</h1>
+		</div>
+
+		
+		<nav><a id="Inicio"></a>
+				<ul>
+					<li><a href="#Inicio">Inicio</a> </li>
+					<li><a href="comp.html">Acerca de</a></li>
+					<li><a href="formulario.php">Lugares turisticos</a></li>
+					<li><a href="comp.html">Comida</a></li>
+				</ul>
+		</nav>
+	</header>
+
+	<section class="main"> 
+		<section class="articles">
+			
+			<article>
+				<h1>¿Qué es tokyo?</h1>
+				<p><p align="justify"><br>
+					<p><p align="justify"> Es la capital del factor 6 de Japón, localizada en el centro-este de la isla de Honshu, concretamente en la región de Kanto. En conjunto forma una de las 47 prefecturas de Japón, aunque su denominación oficial es metrópolis o capital . La ciudad es el centro de la política, economía, educación, comunicación y cultura popular del país. Cuenta también con la mayor concentración de sedes corporativas, instituciones financieras, universidades y colegios, museos, teatros, y establecimientos comerciales y de entretenimiento de todo Japón.<br>
+  				<br>
+  				
+  				Con una población que supera los 37 millones de habitantes, se subdivide en 23 barrios ; 26 ciudades ; un distrito  subdividido en tres pueblos  y una villa ; y cuatro subprefecturas subdivididas en dos pueblos y siete villas, que representan a varias pequeñas islas al sur de Honshu que se extienden más allá de 1800 km de Shinjuku, capital de la metrópoli y sede de la gobernación. El centro de Tokio, con sus 23 barrios, ocupa un tercio de la metrópoli, con una población cercana a los 13,23 millones de habitantes; esta área es lo que se conoce internacionalmente como la ciudad de Tokio.
+    			<br>
+  				<br>
+				  <center>
+ 				 <img src="https://www.vibesofindia.com/wp-content/uploads/2021/07/Tokyo-Street.jpeg" alien="left" width="290" height="208">
+				  <img src="https://cms.finnair.com/resource/blob/720074/0511bb2bc91953d4ef3506d00933819b/tokyo-main-data.jpg" alien="left" width="290" height="208">
+				  <img src="https://www.dianamiaus.com/wp-content/uploads/2019/06/Diana-1084.jpg" alien="left" width="190" height="208">
+				</center>
+			</p></p>
+			</article>
+			<article><a name="historia"></a> 
+				<h2>Historia de tokyo</h2><br> 
+
+				<p> <p align="justify"> A pesar de que desde tiempos antiguos existían pequeñas poblaciones y templos en las colinas cercanas a la bahía de Tokio, se considera que la fundación formal de Tokio fue en 1457, cuando un vasallo del clan Uesugi, Ōta Dōkan construyó el Castillo Edo ; así el área que rodeaba el castillo se comenzó a llamar Edo , literalmente "estuario"). El shogunato Tokugawa, que había tomado el castillo en 1590 y que tenía el control casi absoluto de Japón, estableció su gobierno en Edo en 1603, hecho que dio inicio al Período Edo en la historia japonesa. La nobleza, junto con el Emperador de Japón, permanecieron en Kioto, que siguió siendo la capital oficial, aunque sólo de manera protocolar.
+				<br>
+				<br>
+				A partir de 1872, comenzó a construirse la primera línea de ferrocarril y entre 1885 y 1925 se construyó la Línea Yamanote, línea de ferrocarril urbano que es la más importante de Tokio en la actualidad. En 1889 se estableció la Ciudad de Tokio  con 15 barrios, luego en 1893 los distritos de Tama se unieron a la prefectura. En 1914 se inauguró la Estación de Tokio y en 1927 se inauguró el primer metro subterráneo en la Línea Ginza.
+				<br>
+				<br>
+				El Gran terremoto de Kantō golpeó Tokio en 1923, con un saldo de aproximadamente 143 000 personas fallecidas. Tras la tragedia se inició un plan de reconstrucción que no pudo ser completado debido a su alto coste. A pesar de esto, la ciudad continuó su desarrollo hasta el comienzo de la Segunda Guerra Mundial. En 1936 se inauguró el edificio de la Kokkai (Dieta de Japón); también en ese mismo año ocurrió el Incidente del 26 de febrero, en el que 1500 oficiales del ejército japonés ocuparon el edificio de la Kokkai, el Kantei (Residencia del Primer Ministro) y otros lugares de Tokio en un intento de golpe de Estado, que fue sofocado tres días después.
+				<br>
+				<br>
+				
+				Durante la Segunda Guerra Mundial, Tokio fue intensamente bombardeada a partir de 1942 hasta 1945. A causa de esto, en 1945 la población de Tokio era la mitad que en 1940. Al terminar la guerra, en septiembre de 1945, Tokio fue ocupada militarmente y pasó a ser gobernada por las Fuerzas Aliadas. 
+				<br>
+				<br>
+				</p></p>
+				
+				<img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/Tokyo_Tower_at_night_8.JPG" alien="left" width="280" height="200" text-align="center;">
+				<img src="https://canalhistoria.es/wp-content/uploads/2006/03/Tokyo_1945-3-10-1_portada.png"alien="left" width="280" height="200">
+			    <img src="https://www.gotokyo.org/es/plan/tokyo-outline/images/main.jpg" alien="left" width="190" height="200">
+			</article>
+			<article> <a name="geografia"></a>
+				<h2>Localización geográfica y generalidades</h2><br>
+				<p> <p align="justify"> Tokio está dividido en dos áreas principales: la continental y la insular. El área continental está localizada en el margen noroeste de la bahía de Tokio y está enclavada en el centro-oeste de la isla de Hondo o Honshu, formando parte de la región de Kanto. Las coordenadas del centro de Tokio son 35°41' Norte, 139°46' Este. Limita con la prefectura de Chiba al este, Yamanashi al oeste, Kanagawa al sur y Saitama al norte.
+					<br>
+					<br>
+					El área insular de Tokio abarca dos cadenas de islas en el océano Pacífico, en dirección sur: las islas Izu, que recorren de manera paralela a la península de Izu, en la prefectura de Shizuoka, y las islas Ogasawara que se encuentran a más de mil kilómetros del área continental de Tokio. La más distante es Minami Torishima que está a 1.850 kilómetros.
+
+Tokio incluye lagos, ríos, presas, granjas y parques nacionales, además de las estructuras que han sido construidas por el hombre. Tokio es también parte del Área del Gran Tokio, que incluye las prefecturas de Kanagawa, Saitama y Chiba.
+En la actualidad Tokio es uno de los centros urbanos más importantes del planeta. Es uno de los principales centros financieros y la capital política de Japón. La ciudad tiene un número menor de rascacielos en comparación con otras ciudades de su magnitud, debido principalmente al riesgo de terremotos. Por ello la mayoría de sus edificios no tiene más de 10 pisos. Tokio es también el hogar del sistema de trenes de mayor complejidad en el mundo.
+
+					<br>
+					<br>
+					Su estructura administrativa es similar a la de otras prefecturas japonesas. La región metropolitana de Tokio incluye a 23 Barrios Especiales que, hasta 1943, comprendían la Ciudad de Tokio propiamente dicha. Tokio también tiene 26 ciudades satélites cinco pueblos y ocho villas  cada uno de los cuales tiene gobierno propio.
+					<br>
+					<br>
+					
+						
+					</p> </p>
+					<center>
+					<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/Tokyo_Metropolis_Map.svg/700px-Tokyo_Metropolis_Map.svg.png" width="400" height="300"> 
+					</center>
+			</article>
+			<article> <a id="Demografia"></a>
+				<h2>Demografía</h2><br>
+				<p> <p align="justify">
+				Tokio provee de más puestos de trabajo y lugares de recreación cultural que cualquier otra ciudad de Japón, lo que atrae a muchas personas del resto del país (especialmente jóvenes). Su densidad de población es extremadamente alta: 14 mil personas por kilómetro cuadrado, casi dos veces más que Nueva York, al ser la ciudad más poblada del mundo.
+				<br>
+				<br>
+				El 97 % de la población de la prefectura es descendiente de japoneses. Los dos principales grupos étnicos minoritarios de Tokio son los chinos y coreanos.
+				<br>
+				<br>
+				La religión en Tokio presenta patrones similares al resto del país, donde conviven el budismo, el sintoísmo y otras religiones. Existe un sincretismo constante, donde es frecuente que la población integre dos o más religiones a sus prácticas cotidianas. De las más de nueve mil organizaciones religiosas de la prefectura, el 38 % es budista, el 21 % es sintoísta, y el cristianismo ocupa el 13 %.13​
+
+
+			
+				<br>
+				<br>
+				
+				</p> </p>
+				<center>
+				<img src="https://upload.wikimedia.org/wikipedia/es/timeline/5cf0b16290d4ecdaa994cc1858332870.png" width="400" height="300"> 
+			</center>
+			</article>
+			<article> <a id="vivienda"></a>
+				<h2>Vivienda de tokyo</h2><br>
+				<p><p align="justify">
+					La inmensa población de Tokio ha creado una altísima demanda de residencias. En el pasado, la mayoría de los habitantes de la ciudad vivía en casas de uno o dos pisos, hechas de madera, cada una con su propio jardín, patio y capilla religiosa (llamada Butsudan en los hogares budistas). A medida que la población de Tokio creció, esas casas se demolieron y en su lugar, se construyeron edificios de apartamentos. Dada la inmensa densidad de población de la región, la mayor parte de los apartamentos y casas de la ciudad son pequeñas, y están diseñadas para una familia de dos adultos y dos o tres niños.
+				<br>
+				<br>
+				A pesar de la intensa actividad en la construcción de edificios, la demanda de residencias continuó siendo más alta que la oferta, lo que aumentó los precios del terreno y del alquiler, especialmente dentro de los 23 Barrios Especiales. Como resultado, a partir de la década de 1970, muchas personas abandonaron la región de los 23 Barrios Especiales, mudándose a Tama (parte de la prefectura de Tokio), o incluso a otras ciudades vecinas más distantes.
+				<br>
+				<br>
+				
+			
+				En Tama, el Gobierno Metropolitano de Tokio creó un proyecto de viviendas baratas para familias de bajos ingresos. Sin embargo, estas residencias están localizadas muy lejos de los principales centros comerciales e industriales, y muchos de estos trabajadores pasan más de cuatro horas diarias solamente dentro de algún medio de transporte público.
+				<br>
+				<br>
+				La ciudad de Tokio, en Japón, es uno de los puntos más densamente poblados de la región y del mundo (36 millones de habitantes en su área metropolitana) y se encuentra entre las ciudades más importantes del mundo. Tokio recibe una grande demanda de viviendas anualmente y ya que los lotes prácticamente se acabaron o son muy costosas la ciudad se erige en vertical.
+				<br>
+				<br>
+				</p> </p>
+				<img src="https://upload.wikimedia.org/wikipedia/commons/8/83/Tokyo_from_the_top_of_the_SkyTree_%28cropped%29.JPG" alien="left" width="280" height="200">
+				<img src="https://upload.wikimedia.org/wikipedia/commons/0/0e/Tokyo_Sky_Tree_2012_%E2%85%A3.JPG" alien="left" width="280" height="200"> 
+		
+			</article>
+			
+
+		</section>
+
+		<aside>
+		
+			<p>
+				<br>
+				<a href="#historia">Historia de tokyo</a>
+				<br>
+				<a href="#geografia">Localización geográfica </a>
+				<br>
+				<a href="#Demografia">demografia</a>
+				<br>
+				<a href="#vivienda">Vivienda de tokyo</a>
+				
+				
+				<br>
+				<br>
+				<br>
+			</p>
+				<div>
+					
+					<iframe width="235" height="235" src="https://www.youtube.com/embed/Nm-A89lqnwY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				</div>
+					
+			</aside>
+	
+		
+	<footer>
+		<strong>
+		<p>© 2018-2019 https://es.tokyo.org/Todos los derechos reservados. Todas las marcas registradas son
+			<br> de nuestra compañía en el A.ER. y otros países</strong>
+</p>
+	</footer>
+</body>
+
+</html>
+
+
